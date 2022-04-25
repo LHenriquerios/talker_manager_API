007a1dd9b00e8f19ba72bd2bcecfaee89136cb77
LHenriquerios <henriquerios4@gmail.com>
2022-04-25 17:02:09 -0300

    mais um teste

--- router.js
+++ router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 
-const json = fs.readFileSync('talker.json', 'utf8');
-console.log(json);
-
 const router = express.Router();
 
-router.get('/talker', (_req, res) => res.status(200).send(json));
+const data = fs.readFileSync('talker.json', 'utf8');
+console.log(data);
+
+router.get('/talker', (_req, res) => res.status(200).send(data));
 
 module.exports = router;
