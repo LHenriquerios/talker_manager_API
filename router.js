89109d684683a3641b829f16f1b519fbf6ed3c45
LHenriquerios <henriquerios4@gmail.com>
2022-04-25 19:16:48 -0300

    Agr sim Requisito 1

--- router.js
+++ router.js
@@ -3,12 +3,14 @@ const fs = require('fs').promises;
 
 const router = express.Router();
 
+router.get('/talker', (_req, res) => {
 fs.readFile('talker.json', 'utf8')
 .then((data) => {
-    router.get('/talker', (_req, res) => res.status(200).json(JSON.parse(data)));
+    res.status(200).json(JSON.parse(data));
 })
 .catch((err) => {
  console.log(err);
+});
 });
 
 module.exports = router;
