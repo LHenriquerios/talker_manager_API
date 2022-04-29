3b5e70adf9579bd7ee64dadc581d88f6c9b86be9
LHenriquerios <henriquerios4@gmail.com>
2022-04-29 15:27:38 -0300

    Validação reparada

--- Middlewares/validationInputs.js
+++ Middlewares/validationInputs.js
@@ -27,7 +27,7 @@ const validationAge = (req, res, next) => {
 const validationTalk = (req, res, next) => {
     const { talk } = req.body;
 
-    if (!talk || !talk.watchedAt || !talk.rate) {
+    if (!talk || !talk.watchedAt || talk.rate === undefined) {
         return res.status(BAD_REQUEST).json({
             message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
         });
